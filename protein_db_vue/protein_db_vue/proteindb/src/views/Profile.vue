1042c203e491d5659694656b55eface86e30a213
Diego Hernandez <diego.hernandez.c25@gmail.com>
2019-05-27 23:13:21 +0100

    updated profile

--- protein_db_vue/protein_db_vue/proteindb/src/views/Profile.vue
+++ protein_db_vue/protein_db_vue/proteindb/src/views/Profile.vue
@@ -75,13 +75,13 @@
               <div class="col-sm-9">
               </div>
               <div class="col-sm-1" >
-                <button v-on:click="next()" style="padding-right:30px; background-color: #00B4CC; color: white" type="button" class="btn round  side-edit-btn"><i class="fas fa-arrow-left"></i></button>
+                <button disabled v-on:click="next()" style="padding-right:30px; background-color: #00B4CC; color: white" type="button" class="btn round  side-edit-btn"><i class="fas fa-arrow-left"></i></button>
               </div>
-              <div class="col-sm-1 label-text" style="text-align: center; " >
-                <span style="text-align: center; margin: 50%;" class="label-text"><h2>1</h2></span>
+              <div class="col-sm-1 label-text"  >
+                <span style="text-align: center; " class="label-text"><h2>1</h2></span>
               </div>
               <div class="col-sm-1" >
-                <button  v-on:click="prev()" style="background-color: #00B4CC; color: white; " type="button" class="btn round side-edit-btn"><i class="fas fa-arrow-right"></i></button>
+                <button disabled  v-on:click="prev()" style="background-color: #00B4CC; color: white; " type="button" class="btn round side-edit-btn"><i class="fas fa-arrow-right"></i></button>
               </div>
             </div>
           </div>
